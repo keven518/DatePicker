c834bca91f73021829f09cf35788b16122b3945a
柯文 <kewen@kewendeiMac.local>
2018-04-08 21:40:53 +0800

    0408

---
+++ data.js
@@ -0,0 +1,56 @@
+(function(){
+ var datepicker = {};
+
+ datepicker.getMonthData = function(year, month) {
+
+   var ret = [];
+
+   if(!year || !month) {
+     var today = new Date();
+     year = today.getFullYear();
+     month = today.getMonth() + 1
+   }
+
+   // 第一天
+   var firstDay = new Date(year, month - 1, 1);
+   // 第一天星期几
+   var firstDayWeekDay = firstDay.getDay();
+   if(!firstDayWeekDay) firstDayWeekDay = 7;
+
+   var lastDayOfLastMonth = new Date(year, month - 1, 0)
+   var lastDateOfLastMonth = lastDayOfLastMonth.getDate();
+
+   var preMonthDayCount = firstDayWeekDay - 1; 
+
+   var lastDay = new Date(year, month, 0);
+   var lastDate = lastDay.getDate();
+
+   for(var i=0; i<7*6; i++){
+     var date = i + 1 - preMonthDayCount;
+     var showDate = date;
+     var thisMonth = month;
+     if(date <= 0){
+       // 上一月
+       thisMonth = month - 1;
+       showDate = lastDayOfLastMonth + date;
+     }else if(date > lastDate){
+       // 下一月
+       thisMonth = month + 1;
+       showDate = showDate - lastDate;
+     }
+
+     if(thisMonth === 0) thisMonth = 12;
+     if(thisMonth === 13) thisMonth = 1;
+
+     ret.push({
+       month: thisMonth,
+       date: date,
+       showDate: showDate
+     })
+   }
+
+   return ret;
+ }
+
+ window.datepicker = datepicker;
+})();
